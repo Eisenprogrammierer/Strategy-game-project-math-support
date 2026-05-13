f85f4393ce49ef07aab0fc2097d7fa454783833b
Eisenprogrammierer <eisenprogrammierer@proton.me>
2026-05-13 15:26:09 +0800

    Domains and strategies division by Eisenprogrammierer

---
+++ domains/domains.py
@@ -0,0 +1,13 @@
+
+
+class Entity:
+    def __init__(self):
+        pass
+    def get_mask():
+        pass
+    def set_mask():
+        pass
+    def get_signal():
+        pass
+    def send_signal():
+        pass
